df70c1376b35bec6fda3db5f3c46ef7747cdac76
Yu-Ren-NEU <58605475+Yu-Ren-NEU@users.noreply.github.com>
2020-02-07 16:32:25 -0500

    Create 48.java

---
+++ leetcode/48/48.java
@@ -0,0 +1,16 @@
+//ry
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+        for(int i = 0; i < len / 2; i ++) {
+            for(int j = i; j < len - 1 - i; j ++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[len - 1 - j][i];
+                matrix[len - 1 - j][i] = matrix[len - 1 - i][len - 1 - j];
+                matrix[len - 1 - i][len - 1 - j] = matrix[j][len - 1 - i];
+                matrix[j][len - 1 - i] = tmp;
+            }
+        }
+    }
+}
